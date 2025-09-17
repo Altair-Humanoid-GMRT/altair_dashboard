41a2ba1ba8c359085d63579d64b587272370527b
Falah Naufal Zaki <falahnzk.dev@gmail.com>
2025-09-17 10:15:38 +0700

    update path
    Signed-off-by: Falah Naufal Zaki <falahnzk.dev@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -16,8 +16,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/' : '',
-  basePath: isProd ? '/' : '',
+  // assetPrefix: isProd ? '/altair_dashboard' : '',
+  // basePath: isProd ? '/altair_dashboard' : '',
   output: "export",
 };
 
